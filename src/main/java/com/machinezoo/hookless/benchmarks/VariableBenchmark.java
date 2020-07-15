1265bdcd7c538f2da7326f572c13709674ae69ec
Robert Važan <robert.vazan@protonmail.com>
2020-07-15 19:12:14 +0000

    Annotations and setup/teardown

--- src/main/java/com/machinezoo/hookless/benchmarks/VariableBenchmark.java
+++ src/main/java/com/machinezoo/hookless/benchmarks/VariableBenchmark.java
@@ -1,12 +1,26 @@
 // Part of Hookless: https://hookless.machinezoo.com
 package com.machinezoo.hookless.benchmarks;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import java.util.concurrent.*;
+import org.openjdk.jmh.annotations.*;
 import com.machinezoo.hookless.*;
 
+@State(Scope.Thread)
 public class VariableBenchmark {
 	private ReactiveVariable<String> variable = new ReactiveVariable<>("test");
+	private ReactiveScope scope = new ReactiveScope();
+	private ReactiveScope.Computation computation;
+	@Setup
+	public void setup() {
+		computation = scope.enter();
+	}
+	@TearDown
+	public void teardown() {
+		computation.close();
+	}
 	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@OutputTimeUnit(TimeUnit.NANOSECONDS)
 	public String observeRepeatedly() {
 		return variable.get();
 	}
