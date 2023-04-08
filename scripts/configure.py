ed7bb6c04f719555a9d191ba4be08cf70ae3cc1e
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 12:50:11 +0000

    Updated project configuration: build PRs, updated CONTRIBUTING.md, upgraded stagean, added UA banner

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,33 +1,28 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import os.path
-import sys
-sys.path.append(os.path.normpath(os.path.join(__file__, '../../../project-config/src')))
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-from java import *
-
-project_script_path = __file__
-repository_name = lambda: 'hookless-benchmarks'
-pretty_name = lambda: 'Hookless Benchmarks'
-pom_subgroup = lambda: 'hookless'
-pom_description = lambda: 'JMH benchmarks for Hookless reactive library.'
-inception_year = lambda: 2020
-jdk_version = lambda: 11
-jmh_benchmarks = lambda: True
-stagean_annotations = lambda: True
-is_library = lambda: False
-has_website = lambda: False
-project_status = lambda: f'{experimental_status()} This is a stub project. Thorough testing of Hookless would require many more benchmarks.'
-md_description = lambda: '''\
-    [JMH](https://openjdk.java.net/projects/code-tools/jmh/) benchmarks for [Hookless](https://hookless.machinezoo.com/) reactive programming library.
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'hookless-benchmarks'
+    def pretty_name(self): return 'Hookless Benchmarks'
+    def pom_description(self): return 'JMH benchmarks for Hookless reactive library.'
+    def inception_year(self): return 2020
+    def jmh_benchmarks(self): return True
+    def stagean_annotations(self): return True
+    def is_library(self): return False
+    def has_website(self): return False
+    def project_status(self): return f'{self.experimental_status()} This is a stub project. Thorough testing of Hookless would require many more benchmarks.'
+    def md_description(self): return '''\
+        [JMH](https://openjdk.java.net/projects/code-tools/jmh/) benchmarks for [Hookless](https://hookless.machinezoo.com/) reactive programming library.
+        
+        Latest results are archived in [JMH JSON file](https://cdn.machinezoo.com/jmh/hookless-benchmarks.json).
+        They can be visualized on [jmh.morethan.io](https://jmh.morethan.io/?source=https://cdn.machinezoo.com/jmh/hookless-benchmarks.json).
+    '''
     
-    Latest results are archived in [JMH JSON file](https://cdn.machinezoo.com/jmh/hookless-benchmarks.json).
-    They can be visualized on [jmh.morethan.io](https://jmh.morethan.io/?source=https://cdn.machinezoo.com/jmh/hookless-benchmarks.json).
-'''
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use_hookless()
 
-def dependencies():
-    use_hookless()
-
-generate(globals())
+Project().generate()
